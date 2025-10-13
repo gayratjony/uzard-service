fad1a3e82e0574c0b90f7c253788716cab65fca7
Gayratjon <rayimjonovg@gmail.com>
2025-10-13 22:55:50 +0500

    init

---
+++ src/main/java/uzcard/service/UzcardServiceApplication.java
@@ -0,0 +1,13 @@
+package uzcard.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzcardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UzcardServiceApplication.class, args);
+	}
+
+}
